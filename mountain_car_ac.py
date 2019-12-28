ab0f337542b63801d2da6af23813683ff5263023
elisim <elisimhayev@gmail.com>
2019-12-28 21:58:16 +0200

    continue working

--- mountain_car_ac.py
+++ mountain_car_ac.py
@@ -2,12 +2,13 @@
 import gym
 import numpy as np
 import tensorflow as tf
-import collections
 import datetime
 
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input, Activation
+from keras.layers import Dense, Input
 from keras.optimizers import Adam
+import keras.backend as K
+
 
 np.random.seed(1)
 env = gym.make('MountainCarContinuous-v0')
@@ -27,47 +28,10 @@ class TensorFlowLogger:
         self._file_writer.add_summary(summary, step)
 
 
-class ActorCritic:
-    def __init__(self,
-                 actor_learning_rate, 
-                 critic_learning_rate, 
-                 discount_factor, 
-                 sess, 
-                 tf_logger):
-        self.actor_learning_rate = actor_learning_rate
-        self.critic_learning_rate = critic_learning_rate
-        self.discount_factor = discount_factor
-        self.sess = sess
-        self.tf_logger = tf_logger
-
-        self.actor = PolicyNetwork(self.actor_learning_rate)
-        self.critic = CriticNetwork(self.critic_learning_rate)()
-
-    def train(self, state, action, action_one_hot, reward, next_state, done, total_steps):
-        target = np.zeros((1, 1))
-        advantages = np.zeros((1, self.action_size))
-
-        value = self.critic.predict(state)[0]
-        next_value = self.critic.predict(next_state)[0]
-
-        if done:
-            advantages[0][action] = reward - value
-            target[0][0] = reward
-        else:
-            advantages[0][action] = reward + self.discount_factor * next_value - value
-            target[0][0] = reward + self.discount_factor * next_value
-
-        feed_dict = {self.actor.state: state,
-                     self.actor.R_t: advantages,
-                     self.actor.action: action_one_hot}
-        _, loss = self.sess.run([self.actor.optimizer, self.actor.loss], feed_dict)
-        self.tf_logger.log_scalar(tag='policy_network_loss', value=loss, step=total_steps)
-
-        # self.actor.fit(state, advantages, epochs=1, verbose=0)
-        self.critic.fit(state, target, epochs=1, verbose=0)
-
-
 class CriticNetwork:
+    """
+    Value Network
+    """
     def __init__(self, learning_rate):
         self.critic = Sequential()
         self.critic.add(Dense(units=32, input_dim=state_size, activation='elu'))
@@ -79,29 +43,33 @@ class CriticNetwork:
         return self.critic
 
 
-class PolicyNetwork:
+def PolicyNetwork():
     """
     The network input is the state and output are two scalar functions, μ(s) and σ(s),
     which are used as the mean and standard deviation of a Gaussian (normal) distribution.
     We will choose our actions by sampling from this distribution.
     """
-    def __init__(self, learning_rate, name='policy_network'):
-        self.learning_rate = learning_rate
+    def get_action(state):
+        with tf.variable_scope("policy_network"):
+            init_xavier = tf.contrib.layers.xavier_initializer()
 
-        inp = Input(state_size)
-        x = Dense(units=32, activation='elu')(inp)
-        x = Dense(units=32, activation='elu')(x)
-        sigma = Dense(units=1, activation='softplus')(x) + 1e-5  # σ
-        mu = Dense(units=1)(x)  # μ
-        self.model = Model(int, [sigma, mu])
+            hidden1 = tf.layers.dense(state, 32, tf.nn.elu, init_xavier)
+            hidden2 = tf.layers.dense(hidden1, 32, tf.nn.elu, init_xavier)
+            mu = tf.layers.dense(hidden2, 1, init_xavier)
+            sigma = tf.layers.dense(hidden2, 1, tf.nn.softplus, init_xavier)
+            sigma = tf.nn.softplus(sigma) + 1e-5
 
-    def get_action(self, state):
-        sigma, mu = self.model.predict(state, verbose=0)
-        norm_dist = tf.contrib.distributions.Normal(mu, sigma)
-        action = tf.squeeze(norm_dist.sample(1), axis=0)
-        # Clips tensor values to a env min and max values
-        action = tf.clip_by_value(action, env.action_space.low[0], env.action_space.high[0])
-        return action
+            norm_dist = tf.contrib.distributions.Normal(mu, sigma)
+            action_tf_var = tf.squeeze(norm_dist.sample(1), axis=0)
+            action_tf_var = tf.clip_by_value(action_tf_var, env.action_space.low[0], env.action_space.high[0])
+
+        return action_tf_var, norm_dist
+
+    def minimize():
+        # define actor (policy) loss function
+        loss_actor = -tf.log(norm_dist.prob(action_placeholder) + 1e-5) * delta_placeholder
+        training_op_actor = tf.train.AdamOptimizer(
+            lr_actor, name='actor_optimizer').minimize(loss_actor)
 
 
 def main():
@@ -117,13 +85,10 @@ def main():
     tf_logger = TensorFlowLogger()
 
     # actor critic
-    actor = PolicyNetwork(actor_learning_rate)
-    critic = CriticNetwork(critic_learning_rate)
+    action_tf_var, norm_dist = policy_network(state_placeholder)
+    critic = CriticNetwork(critic_learning_rate)()
 
-
-    # model = ActorCritic(policy_learning_rate, critic_learning_rate, discount_factor, tf_logger)
     solved = False
-    Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
     episode_rewards = np.zeros(max_episodes)
     average_rewards = 0.0
     total_steps = 0
@@ -135,30 +100,46 @@ def main():
 
         # Generate an episode, out is list of episode_transitions: state, action, reward, next_state and done.
         for step in range(max_steps):
-            action = actor.get_action(state)
-            actions_distribution = sess.run(model.actor.actions_distribution, feed_dict={model.actor.state: state})
-            action = np.random.choice(np.arange(len(actions_distribution)), p=actions_distribution)
+
+            # action.shape = (1,1)
+            # action = actor.get_action(state) #### TODO
+
+            # Execute action and observe reward & next state from env
             next_state, reward, done, _ = env.step(action)
-            next_state = next_state.reshape([1, state_size])
-
-            if render:
-                env.render()
-
-            action_one_hot = np.zeros(action_size)
-            action_one_hot[action] = 1
-            episode_transitions.append(
-                Transition(state=state, action=action_one_hot, reward=reward, next_state=next_state, done=done))
             episode_rewards[episode] += reward
 
-            model.train(state, action, action_one_hot, reward, next_state, done, total_steps)
+            # V_of_next_state.shape=(1,1)
+            V_of_next_state = critic.predict(next_state)
+
+            # Set TD Target: target = r + gamma * V(next_state)
+            target = reward + discount_factor * np.squeeze(V_of_next_state)
+
+            # td_error = target - V(s)
+            V_of_curr_state = critic.predict(state)
+            td_error = target - np.squeeze(V_of_curr_state)
+
+            # Update actor
+            actor.model.fit()
+
+            # Update critic
+            critic.fit(target, V_of_curr_state)
+
+
+            # Update actor by minimizing loss (Actor training)
+            _, loss_actor_val = sess.run(
+                [training_op_actor, loss_actor],
+                feed_dict={action_placeholder: np.squeeze(action),
+                           state_placeholder: scale_state(state),
+                           delta_placeholder: td_error})
+            if render:
+                env.render()
 
             if done:
                 if episode > 98:
                     # Check if solved
                     average_rewards = np.mean(episode_rewards[(episode - 99):episode + 1])
                 print(
-                    "Episode {} Reward: {} Average over 100 episodes: {}".format(episode, episode_rewards[episode],
-                                                                                 round(average_rewards, 2)))
+                    "Episode {} Reward: {} Average over 100 episodes: {}".format(episode, episode_rewards[episode], round(average_rewards, 2)))
                 if average_rewards > 475:
                     print(' Solved at episode: ' + str(episode))
                     print(' Total Steps: ' + str(total_steps))
