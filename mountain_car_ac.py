699c3a123fb72f6601e8b0a2ed368199cf86b45d
elisim <elisimhayev@gmail.com>
2019-12-29 11:27:38 +0200

    DONE writing mountain. need to run

--- mountain_car_ac.py
+++ mountain_car_ac.py
@@ -4,11 +4,10 @@ import numpy as np
 import tensorflow as tf
 import datetime
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Input
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.optimizers import Adam
-import keras.backend as K
-
+import sklearn.preprocessing
 
 np.random.seed(1)
 env = gym.make('MountainCarContinuous-v0')
@@ -19,8 +18,6 @@ action_size = env.action_space.shape[0]
 class TensorFlowLogger:
     def __init__(self, with_baseline):
         self._log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-#         if with_baseline:
-#             self._log_dir += "_with_baseline"
         self._file_writer = tf.summary.FileWriter(self._log_dir + "/metrics")
 
     def log_scalar(self, tag, value, step):
@@ -32,6 +29,7 @@ class CriticNetwork:
     """
     Value Network
     """
+
     def __init__(self, learning_rate):
         self.critic = Sequential()
         self.critic.add(Dense(units=32, input_dim=state_size, activation='elu'))
@@ -43,114 +41,156 @@ class CriticNetwork:
         return self.critic
 
 
-def PolicyNetwork():
+class PolicyNetwork:
     """
     The network input is the state and output are two scalar functions, μ(s) and σ(s),
     which are used as the mean and standard deviation of a Gaussian (normal) distribution.
     We will choose our actions by sampling from this distribution.
     """
-    def get_action(state):
+
+    def __init__(self, tf_session, learning_rate):
+        self.tf_session = tf_session
+        self.learning_rate = learning_rate
+
+        # placeholders to be fed
+        self.state_placeholder = tf.placeholder(tf.float32, [None, 2])
+        self.td_error_placeholder = tf.placeholder(tf.float32)
+        self.action_placeholder = tf.placeholder(tf.float32)
+
+        # build model
         with tf.variable_scope("policy_network"):
             init_xavier = tf.contrib.layers.xavier_initializer()
 
-            hidden1 = tf.layers.dense(state, 32, tf.nn.elu, init_xavier)
+            # two hidden layers
+            hidden1 = tf.layers.dense(self.state_placeholder, 32, tf.nn.elu, init_xavier)
             hidden2 = tf.layers.dense(hidden1, 32, tf.nn.elu, init_xavier)
+
+            # two outputs: mu and sigma
             mu = tf.layers.dense(hidden2, 1, init_xavier)
-            sigma = tf.layers.dense(hidden2, 1, tf.nn.softplus, init_xavier)
+            sigma = tf.layers.dense(hidden2, 1, init_xavier)
             sigma = tf.nn.softplus(sigma) + 1e-5
 
+            # create normal distribution and get action variable
             norm_dist = tf.contrib.distributions.Normal(mu, sigma)
             action_tf_var = tf.squeeze(norm_dist.sample(1), axis=0)
             action_tf_var = tf.clip_by_value(action_tf_var, env.action_space.low[0], env.action_space.high[0])
+            self.action_tf_var = action_tf_var
 
-        return action_tf_var, norm_dist
+        # define actor loss function and training operation
+        self.loss = -tf.log(norm_dist.prob(self.action_placeholder) + 1e-5) * self.td_error_placeholder
+        self.training_op = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
 
-    def minimize():
-        # define actor (policy) loss function
-        loss_actor = -tf.log(norm_dist.prob(action_placeholder) + 1e-5) * delta_placeholder
-        training_op_actor = tf.train.AdamOptimizer(
-            lr_actor, name='actor_optimizer').minimize(loss_actor)
+    def get_action(self, state):
+        action = self.sess.run(self.action_tf_var, feed_dict={self.state_placeholder: state})
+        return action
+
+    def fit(self, action, state, td_error):
+        # Update actor by minimizing loss
+        self.sess.run([self.training_op, self.loss],
+                      feed_dict={self.action_placeholder: action, self.state_placeholder: state,
+                                 self.td_error_placeholder: td_error})
+
+    def save_model(self):
+        pass
+
+    def load_model(self):
+        pass
 
 
 def main():
-    # Define hyperparameters
-    max_episodes = 5000
-    max_steps = 501
-    discount_factor = 0.99
-    actor_learning_rate = 0.0004
-    critic_learning_rate = 0.0004
-    render = False
+    with tf.Session() as sess:
 
-    # Create Logger to log scalars
-    tf_logger = TensorFlowLogger()
+        # Define hyperparameters
+        max_episodes = 5000
+        max_steps = 501
+        discount_factor = 0.99
+        actor_learning_rate = 0.0004
+        critic_learning_rate = 0.0004
 
-    # actor critic
-    action_tf_var, norm_dist = policy_network(state_placeholder)
-    critic = CriticNetwork(critic_learning_rate)()
+        # Create Logger to log scalars and StateScaler to scale to states
+        tf_logger = TensorFlowLogger()
+        state_scaler = StateScaler()
 
-    solved = False
-    episode_rewards = np.zeros(max_episodes)
-    average_rewards = 0.0
-    total_steps = 0
+        # actor critic
+        actor = PolicyNetwork(sess, actor_learning_rate)
+        critic = CriticNetwork(critic_learning_rate)()
 
-    for episode in range(max_episodes):
-        steps_per_episode = 0
-        state = env.reset()
-        episode_transitions = []
+        solved = False
+        episode_rewards = np.zeros(max_episodes)
+        average_rewards = 0.0
+        total_steps = 0
 
-        # Generate an episode, out is list of episode_transitions: state, action, reward, next_state and done.
-        for step in range(max_steps):
+        for episode in range(max_episodes):
+            steps_per_episode = 0
+            state = env.reset()
 
-            # action.shape = (1,1)
-            # action = actor.get_action(state) #### TODO
+            # Generate an episode, out is list of episode_transitions: state, action, reward, next_state and done.
+            for step in range(max_steps):
+                # action.shape = (1,1)
+                action = actor.get_action(state_scaler.scale(state))
 
-            # Execute action and observe reward & next state from env
-            next_state, reward, done, _ = env.step(action)
-            episode_rewards[episode] += reward
+                # Execute action and observe reward & next state from env
+                # next_state shape=(2,)
+                # env.step() requires input shape = (1,)
+                next_state, reward, done, _ = env.step(np.squeeze(action))
+                episode_rewards[episode] += reward
 
-            # V_of_next_state.shape=(1,1)
-            V_of_next_state = critic.predict(next_state)
+                # V_of_next_state.shape=(1,1)
+                V_of_next_state = critic.predict(state_scaler.scale(next_state))
 
-            # Set TD Target: target = r + gamma * V(next_state)
-            target = reward + discount_factor * np.squeeze(V_of_next_state)
+                # Set TD Target: target = r + gamma * V(next_state)
+                target = reward + discount_factor * tf.squeeze(V_of_next_state)
 
-            # td_error = target - V(s)
-            V_of_curr_state = critic.predict(state)
-            td_error = target - np.squeeze(V_of_curr_state)
+                # td_error = target - V(s)
+                V_of_curr_state = critic.predict(state_scaler.scale(state))
+                td_error = target - np.squeeze(V_of_curr_state)
 
-            # Update actor
-            actor.model.fit()
+                # Update actor
+                actor.fit(sess, action=np.squeeze(action), state=state_scaler.scale(state), td_error=td_error)
 
-            # Update critic
-            critic.fit(target, V_of_curr_state)
+                # Update critic
+                critic.fit(target, V_of_curr_state, verbose=0)
 
-
-            # Update actor by minimizing loss (Actor training)
-            _, loss_actor_val = sess.run(
-                [training_op_actor, loss_actor],
-                feed_dict={action_placeholder: np.squeeze(action),
-                           state_placeholder: scale_state(state),
-                           delta_placeholder: td_error})
-            if render:
-                env.render()
-
-            if done:
-                if episode > 98:
+                if done:
                     # Check if solved
-                    average_rewards = np.mean(episode_rewards[(episode - 99):episode + 1])
-                print(
-                    "Episode {} Reward: {} Average over 100 episodes: {}".format(episode, episode_rewards[episode], round(average_rewards, 2)))
-                if average_rewards > 475:
-                    print(' Solved at episode: ' + str(episode))
-                    print(' Total Steps: ' + str(total_steps))
-                    solved = True
+                    if episode > 98:
+                        average_rewards = np.mean(episode_rewards[(episode - 99):episode + 1])
+                    if average_rewards > 90:
+                        print('Solved at episode: ' + str(episode))
+                        print('Total Steps: ' + str(total_steps))
+                        solved = True
+                    break
+                state = next_state
+                steps_per_episode += 1
+
+            print(f"Episode: {episode}, Number of Steps : {steps_per_episode}, Cumulative reward: {episode_rewards[episode]:0.2f}")
+            tf_logger.log_scalar(tag='average_100_episodes_reward', value=average_rewards, step=episode)
+            tf_logger.log_scalar(tag='episode_reward', value=episode_rewards[episode], step=episode)
+            tf_logger.log_scalar(tag='steps_per_episode', value=steps_per_episode, step=episode)
+
+            if solved:
                 break
-            state = next_state
-            steps_per_episode += 1
 
-        tf_logger.log_scalar(tag='average_100_episodes_reward', value=average_rewards, step=episode)
-        tf_logger.log_scalar(tag='episode_reward', value=episode_rewards[episode], step=episode)
-        tf_logger.log_scalar(tag='steps_per_episode', value=steps_per_episode, step=episode)
 
-        if solved:
-            break
+class StateScaler:
+    """
+    scale and normalize the state (subtracts the mean and normalizes states to unit variance).
+    """
+    def __init__(self, n_samples=10000):
+        state_samples = np.array([env.observation_space.sample() for _ in range(n_samples)])
+        self.scaler = sklearn.preprocessing.StandardScaler()
+        self.scaler.fit(state_samples)
+
+    def scale(self, state):
+        """
+        input shape = (2,)
+        output shape = (1,2)
+        """
+        scaled = self.scaler.transform([state])
+        return scaled
+
+
+
+
+
+
